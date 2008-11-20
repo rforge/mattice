6a6693fab5d0a3ee08ce414de264253fe1d6e4a7
andrew_hipp <andrew_hipp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-20 21:57:37 +0000

    corrected informationCriterion.R so that introduction of NAs doesn't cause an error
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mattice@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/informationCriterion.R
+++ pkg/R/informationCriterion.R
@@ -8,18 +8,18 @@ informationCriterion <- function(u = NULL, lnL = NULL, K, n = 0, names = NULL) {
     AICc[i] <- u[i] + (2 * K[i] * (n / (n - K[i] - 1)))
     AIC[i] <- u[i] + (2 * K[i])
     BIC[i] <- u[i] + (log(n) * K[i]) }
-  deltaAIC <- as.vector(lapply(AIC, function(x, allX) {x - min(allX)}, allX = AIC), mode = "numeric")
-  deltaAICc <- as.vector(lapply(AICc, function(x, allX) {x - min(allX)}, allX = AICc), mode = "numeric")
-  deltaBIC <- as.vector(lapply(BIC, function(x, allX) {x - min(allX)}, allX = BIC), mode = "numeric")
-  AICwi <- as.vector(lapply(deltaAIC, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta))}, allDelta = deltaAIC), mode = "numeric")
-  AICcwi <- as.vector(lapply(deltaAICc, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta))}, allDelta = deltaAICc), mode = "numeric")
-  BICwi <- as.vector(lapply(deltaBIC, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta))}, allDelta = deltaBIC), mode = "numeric")
+  deltaAIC <- as.vector(lapply(AIC, function(x, allX) {x - min(allX, na.rm = T)}, allX = AIC), mode = "numeric")
+  deltaAICc <- as.vector(lapply(AICc, function(x, allX) {x - min(allX, na.rm = T)}, allX = AICc), mode = "numeric")
+  deltaBIC <- as.vector(lapply(BIC, function(x, allX) {x - min(allX, na.rm = T)}, allX = BIC), mode = "numeric")
+  AICwi <- as.vector(lapply(deltaAIC, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta), na.rm = T)}, allDelta = deltaAIC), mode = "numeric")
+  AICcwi <- as.vector(lapply(deltaAICc, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta), na.rm = T)}, allDelta = deltaAICc), mode = "numeric")
+  BICwi <- as.vector(lapply(deltaBIC, function(x, allDelta) {exp(-0.5 * x) / sum(exp(-0.5 * allDelta), na.rm = T)}, allDelta = deltaBIC), mode = "numeric")
   return(list(names = names, u = u, K = K, AIC = AIC, AICc = AICc, BIC = BIC, AICwi = AICwi, AICcwi = AICcwi, BICwi = BICwi)) }
 
 informationCriterion.hansenBatch <- function(hansenBatch) {
 ## call informationCriterion for a 'hansen.batch' object
 ## Just returns AIC, AICc, and BIC weights for each of the trees analyzed in a hansenBatch object
-  outdata <- list(length(hansenBatch$hansens))
+  outdata <- vector("list", length(hansenBatch$hansens))
   N = hansenBatch$N
   for(i in 1:length(outdata)) {
     temp <- hansenBatch$hansens[[i]]
