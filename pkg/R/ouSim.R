6c37dd292efd6a42fd30411e17b64db45560faf5
andrew_hipp <andrew_hipp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-03 21:16:14 +0000

    misc. simulation updates
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mattice@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ouSim.R
+++ pkg/R/ouSim.R
@@ -1,4 +1,4 @@
-ouSim <- function(phy, rootState = 0, shiftBranches = NULL, shiftStates = NULL, alpha = 0, variance = 1, theta = rootState, model = "OU", branchMeans = NULL, steps = 1000) {
+ouSim.phylo <- function(phy, rootState = 0, shiftBranches = NULL, shiftStates = NULL, alpha = 0, variance = 1, theta = rootState, model = "OU", branchMeans = NULL, steps = 1000) {
 ## function to plot a simulated dataset under brownian motion or Ornstein-Uhlenbeck (OU) model
 ## Arguments:
 ##   phy is an ape-style tree
@@ -10,6 +10,8 @@ ouSim <- function(phy, rootState = 0, shiftBranches = NULL, shiftStates = NULL, 
 ##  "platt" is the Platt-Molitor model, in which the only phylogenetic effect is the mean and variance for a given branch
 ## Andrew Hipp (ahipp@mortonarb.org), January 2008 
 ## July 2008: modified to accomodate a vector of alpha and theta corresponding to branches
+## Dec 2008: This function I'm leaving as is for the time being and just letting the phylo method be as raw as always.
+##           New developments will be in the ouchtree, brown, hansen, and hansenBatch methods
 
 preorderOU <- function(branchList, phy, startNode, startState, alpha, theta) {
 ## Recursive function to generate the data under a Brownian motion or OU model (not needed in the Platt model)
--- pkg/R/ouSimHead.R
+++ pkg/R/ouSimHead.R
@@ -1,9 +1,10 @@
 ouSim <- function(tree, ...) {
+# right now this is just a switcher, but eventually these should be turned into proper methods of a generic ouSim
   switch(class(tree), 
          phylo = ouSim.phylo(tree, ...),
          ouchtree = ouSim.ouchtree(tree, ...),
-         brown = ouSim.brown(tree, ...),
-         hansen = ouSim.hansen(tree, ...),
+         brown = ouSim.brownHansen(tree, ...),
+         hansen = ouSim.brownHansen(tree, ...),
          batchHansen = ouSim.batchHansen(tree, ...),
          "Unrecognized tree class"
          )
