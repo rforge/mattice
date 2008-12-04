d61f2dd59bbc188d3f0304416397112ce28ab906
andrew_hipp <andrew_hipp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-12-04 11:58:55 +0000

    continuing to ouchtree-compatibilize ouSim
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mattice@73 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ouchSim.R
+++ pkg/R/ouchSim.R
@@ -1,18 +1,45 @@
-ouchSim <- function(tree, rootState = 0, shiftBranches = NULL, shiftStates = NULL, alpha = 0, variance = 1, theta = rootState, steps = 1000) {
+ouchSim.ouchtree <- function(tree, rootState = 0, shiftBranches = NULL, shiftStates = NULL, alpha = 0, variance = 1, theta = rootState, steps = 1000) {
 ## function to plot a simulated dataset under brownian motion or Ornstein-Uhlenbeck (OU) model
 ## Arguments:
-##   tree is an ouch-style tree
+##   tree is an ouch-style (S4) tree
 ##   alpha and theta are either single values or vectors of length (length(branchList))
 ##   shiftBranches is a vector indicating any branches at which an OU or brownian motion model has a determined shift in ancestral state
 ##   shiftStates is a vector of length = length(shiftBranches) indicaing the ancestral states for the determined break points
-## Models:
-##  "OU" is a brownian motion or OU model 
-##  "platt" is the Platt-Molitor model, in which the only phylogenetic effect is the mean and variance for a given branch
-## Andrew Hipp (ahipp@mortonarb.org), January 2008 
-## July 2008: modified to accomodate a vector of alpha and theta corresponding to branches
+
+## 1. initialize
+
+if(length(alpha) == 1) alpha <- rep(alpha, tree@nnodes)
+if(length(theta) == 1) theta <- rep(theta, tree@nnodes)
+## The following creates a list of random draws from the normal distribution, with standard deviation scaled by total tree length and the number of draws for each branch equal to the number of steps in that branch. If there is a separate variance for each branch, I assume the variance is expressed in tree-length units, not branch-length units, so the scaling is the same for all branches (viz., sd = sqrt(variance / steps))
+if(length(variance) == 1) branchList <- lapply(round(phy$edge.length*steps), rnorm, sd = sqrt(variance/steps))
+else {
+  branchList <- vector("list", length(phy$edge.length))
+  brLengths <- round(phy$edge.length*steps)
+  brSD <- sqrt(variance/steps)
+  for(i in seq(length(brLengths))) branchList[[i]] <- rnorm(n = brLengths[i], mean = 0, sd = brSD[i])
+  }
+timesList <- lapply(branchList, seq)
+rootNode = length(phy$tip.label) + 1
+
+startTimes <- max(branching.times(phy)) - branching.times(phy) ## WORKS, BUT ASSUMES ULTRAMETRICITY
+startTimes <- round(startTimes[match(phy$edge[,1], names(startTimes))] * steps)
+for (i in 1:length(timesList)) timesList[[i]] <- timesList[[i]] + startTimes[i]
+
+## 3. traverse
+for(i in which(phy$edge[, 1] == rootNode)) {
+  branchList <- preorderOU(branchList, phy, phy$edge[i,2], rootState, alpha, theta) 
+  }
+
+value <- (list(branchList = branchList, timesList = timesList, steps = steps, parameters = list(rootState = rootState, alpha = alpha, variance = variance, theta = theta))) 
+class(value) <- "ouSim"
+return(value)}
 
 preorderOU <- function(branchList, phy, startNode, startState, alpha, theta) {
-## Recursive function to generate the data under a Brownian motion or OU model (not needed in the Platt model)
+## Recursive function to generate the data under a Brownian motion or OU model
+## modified for ouchtree (s4) Dec 08
+## branch times back from each tip are in tree@epochs, indexed by tip number
+## nodes back from each node (including tips) are in tree@lineages, indexed by node number
+## a branch length is the time of a node - the time of its ancestor
   startBranch = which(phy$edge[,2] == startNode)
   if(!identical(shiftStates, NULL)) {
     if(startBranch %in% shiftBranches) startState <- shiftStates[match(startBranch, shiftBranches)] }
@@ -26,42 +53,13 @@ preorderOU <- function(branchList, phy, startNode, startState, alpha, theta) {
     for(i in daughterBranches) branchList <- preorderOU(branchList, phy, i, endState, alpha, theta) }
   return(branchList) }  
 
-## 1. initialize
-
-if(length(alpha) == 1) alpha <- rep(alpha, length(phy$edge.length))
-if(length(theta) == 1) theta <- rep(theta, length(phy$edge.length))
-## The following creates a list of random draws from the normal distribution, with standard deviation scaled by total tree length and the number of draws for each branch equal to the number of steps in that branch. If there is a separate variance for each branch, I assume the variance is expressed in tree-length units, not branch-length units, so the scaling is the same for all branches (viz., sd = sqrt(variance / steps))
-if(model == "OU") {
-	if(length(variance) == 1) branchList <- lapply(round(phy$edge.length*steps), rnorm, sd = sqrt(variance/steps))
-	  else {
-	    branchList <- vector("list", length(phy$edge.length))
-	    brLengths <- round(phy$edge.length*steps)
-	    brSD <- sqrt(variance/steps)
-	    for(i in seq(length(brLengths))) branchList[[i]] <- rnorm(n = brLengths[i], mean = 0, sd = brSD[i]) }}
-if(model == "platt") {
-	if(length(variance) == 1) variance <- rep(variance, length(phy$edge.length))
-	branchList <- vector("list", length(phy$edge.length))
-	if(length(branchMeans) == 1) branchMeans <- rep(branchMeans, length(phy$edge))
-	brLengths <- round(phy$edge.length*steps)
-	brSD <- sqrt(variance)
-	for(i in seq(length(brLengths))) branchList[[i]] <- rnorm(n = brLengths[i], mean = branchMeans[i], sd = brSD[i])
-	}
-timesList <- lapply(branchList, seq)
-rootNode = length(phy$tip.label) + 1
-
-startTimes <- max(branching.times(phy)) - branching.times(phy) ## WORKS, BUT ASSUMES ULTRAMETRICITY
-startTimes <- round(startTimes[match(phy$edge[,1], names(startTimes))] * steps)
-for (i in 1:length(timesList)) timesList[[i]] <- timesList[[i]] + startTimes[i]
-
-## 3. traverse
-if(model == "OU") {
-	for(i in which(phy$edge[, 1] == rootNode)) {
-	  branchList <- preorderOU(branchList, phy, phy$edge[i,2], rootState, alpha, theta) }}
-if(model == "platt") branchList <- branchList
-
-value <- (list(branchList = branchList, timesList = timesList, steps = steps, parameters = list(rootState = rootState, alpha = alpha, variance = variance, theta = theta))) 
-class(value) <- "ouSim"
-return(value)}
+branchLength <- function(tree, endNode) {
+  endNode <- as.character(endNode)
+  ancestor <- tree@ancestors[which(tree@nodes == endNode)]
+  endTime <- ot@times[which(ot@nodes == endNode)]
+  startTime <- ot@times[which(ot@nodes == ancestor)]
+  return(endTime - startTime)
+}
 
 plot.ouSim <- function(ouSim, nodeColor = "blue", nodeDotSize = 1.4, colors = rep("black", length(ouSim$branchList)), ...) {
 ## Plot the data by calling plot(ouSimObject)
