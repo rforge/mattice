b6052bad7116394f0617423497531067d83f870c
andrew_hipp <andrew_hipp@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-03-22 16:53:41 +0000

    Fixing ouSim so that it uses alpha, not sqrt.alpha; this was causing an error in the vignette
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mattice@251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/ouSim.hansenBatch.R
+++ pkg/R/ouSim.hansenBatch.R
@@ -1,9 +1,9 @@
 ouSim.hansenSummary <- function(object, tree, treeNum = 1, rootState = NULL, ...) {
-## runs ouSim.ouchtree for a hansenBatch or hansenSummary object, using the model-averaged sqrt.alpha, sigma.squared, and theta vector from one tree
+## runs ouSim.ouchtree for a hansenBatch or hansenSummary object, using the model-averaged alpha, sigma.squared, and theta vector from one tree
   analysis <- object
   # if(class(analysis) == "hansenBatch") analysis <- summary(analysis)
   if(identical(rootState, NULL)) rootState <- analysis$thetaMatrix[treeNum, ][tree@root] # rootstate taken to be the optimum at the root
-  outdata <- ouSim(tree, rootState, sqrt.alpha = analysis$modelAvgAlpha, variance = analysis$modelAvgSigmaSq, theta = analysis$thetaMatrix[treeNum, ], ...)
+  outdata <- ouSim(tree, rootState, alpha = analysis$modelAvgAlpha, variance = analysis$modelAvgSigmaSq, theta = analysis$thetaMatrix[treeNum, ], ...)
   class(outdata) <- "ouSim"
   return(outdata)
 }
@@ -14,13 +14,13 @@ ouSim.hansentree <- function(object, ...) {
   analysis <- object
   su <- summary(analysis)
   if(length(analysis@regimes) > 1) warning("Theta is based on analysis@regimes[[1]]")
-  if(dim(su$sqrt.alpha)[1] != 1) stop("This is a one-character simulation; analysis appears to be based on > 1 character")
-  sqrt.alpha <- as.vector(su$sqrt.alpha)
+  if(dim(su$alpha)[1] != 1) stop("This is a one-character simulation; analysis appears to be based on > 1 character")
+  alpha <- as.vector(su$alpha)
   theta <- su$optima[[1]][analysis@regimes[[1]]]
   rootState <- theta[analysis@root] # rootstate taken to be the optimum at the root
   variance <- as.vector(su$sigma.squared)
   tree <- ouchtree(analysis@nodes, analysis@ancestors, analysis@times) 
-  outdata <- ouSim.ouchtree(tree, rootState, sqrt.alpha, variance, theta, ...)
+  outdata <- ouSim.ouchtree(tree, rootState, alpha, variance, theta, ...)
   outdata$colors <- analysis@regimes[[1]]
   class(outdata) <- "ouSim"
   return(outdata)
@@ -30,13 +30,13 @@ ouSim.browntree <- function(object, ...) {
   analysis <- object
   su <- summary(analysis)
   if(length(analysis@regimes) > 1) warning("Theta is based on analysis@regimes[[1]]")
-  if(dim(su$sqrt.alpha)[1] != 1) stop("This is a one-character simulation; analysis appears to be based on > 1 character")
-  sqrt.alpha <- 0
+  if(dim(su$alpha)[1] != 1) stop("This is a one-character simulation; analysis appears to be based on > 1 character")
+  alpha <- 0
   theta <- 0
   rootState <- su$theta[[1]]
   variance <- as.vector(su$sigma.squared)
   tree <- ouchtree(analysis@nodes, analysis@ancestors, analysis@times) 
-  outdata <- ouSim.ouchtree(tree, rootState, sqrt.alpha, variance, theta, ...)
+  outdata <- ouSim.ouchtree(tree, rootState, alpha, variance, theta, ...)
   outdata$colors <- analysis@regimes[[1]]
   class(outdata) <- "ouSim"
   return(outdata)
--- pkg/R/ouSim.phylo.R
+++ pkg/R/ouSim.phylo.R
@@ -1,19 +1,19 @@
-ouSim.phylo <- function(object, rootState = 0, shiftBranches = NULL, shiftStates = NULL, sqrt.alpha = 0, variance = 1, theta = rootState, model = "OU", branchMeans = NULL, steps = 1000, ...) {
+ouSim.phylo <- function(object, rootState = 0, shiftBranches = NULL, shiftStates = NULL, alpha = 0, variance = 1, theta = rootState, model = "OU", branchMeans = NULL, steps = 1000, ...) {
 ## function to plot a simulated dataset under brownian motion or Ornstein-Uhlenbeck (OU) model
 ## Arguments:
 ##   phy is an ape-style tree
-##   sqrt.alpha and theta are either single values or vectors of length (length(branchList))
+##   alpha and theta are either single values or vectors of length (length(branchList))
 ##   shiftBranches is a vector indicating any branches at which an OU or brownian motion model has a determined shift in ancestral state
 ##   shiftStates is a vector of length = length(shiftBranches) indicaing the ancestral states for the determined break points
 ## Models:
 ##  "OU" is a brownian motion or OU model 
 ##  "meanVar" is a model in which the only phylogenetic effect is the mean and variance for a given branch
 ## Andrew Hipp (ahipp@mortonarb.org), January 2008 
-## July 2008: modified to accomodate a vector of sqrt.alpha and theta corresponding to branches
+## July 2008: modified to accomodate a vector of alpha and theta corresponding to branches
 ## Dec 2008: This function I'm leaving as is for the time being and just letting the phylo method be as raw as always.
 ##           New developments will be in the ouchtree, brown, hansen, and hansenBatch methods
 phy <- object
-preorderOU <- function(branchList, phy, startNode, startState, sqrt.alpha, theta) {
+preorderOU <- function(branchList, phy, startNode, startState, alpha, theta) {
 ## Recursive function to generate the data under a Brownian motion or OU model (not needed in the Platt model)
   startBranch = which(phy$edge[,2] == startNode)
   if(!identical(shiftStates, NULL)) {
@@ -21,16 +21,16 @@ preorderOU <- function(branchList, phy, startNode, startState, sqrt.alpha, theta
   message(paste('Working on branch',startBranch,'with starting state',startState))
   branchList[[startBranch]][1] <- startState
   for (i in 2:length(branchList[[startBranch]])) {
-    branchList[[startBranch]][i] <- branchList[[startBranch]][i - 1] + branchList[[startBranch]][i] + sqrt.alpha[startBranch] / steps * (theta[startBranch] - branchList[[startBranch]][i - 1]) }
+    branchList[[startBranch]][i] <- branchList[[startBranch]][i - 1] + branchList[[startBranch]][i] + alpha[startBranch] / steps * (theta[startBranch] - branchList[[startBranch]][i - 1]) }
   endState = branchList[[startBranch]][length(branchList[[startBranch]])]
   daughterBranches <- phy$edge[which(phy$edge[, 1] == startNode), 2]
   if(!identical(as.integer(daughterBranches), integer(0))) {
-    for(i in daughterBranches) branchList <- preorderOU(branchList, phy, i, endState, sqrt.alpha, theta) }
+    for(i in daughterBranches) branchList <- preorderOU(branchList, phy, i, endState, alpha, theta) }
   return(branchList) }  
 
 ## 1. initialize
 
-if(length(sqrt.alpha) == 1) sqrt.alpha <- rep(sqrt.alpha, length(phy$edge.length))
+if(length(alpha) == 1) alpha <- rep(alpha, length(phy$edge.length))
 if(length(theta) == 1) theta <- rep(theta, length(phy$edge.length))
 ## The following creates a list of random draws from the normal distribution, with standard deviation scaled by total tree length and the number of draws for each branch equal to the number of steps in that branch. If there is a separate variance for each branch, I assume the variance is expressed in tree-length units, not branch-length units, so the scaling is the same for all branches (viz., sd = sqrt(variance / steps))
 if(model == "OU") {
@@ -58,10 +58,10 @@ for (i in 1:length(timesList)) timesList[[i]] <- timesList[[i]] + startTimes[i]
 ## 3. traverse
 if(model == "OU") {
 	for(i in which(phy$edge[, 1] == rootNode)) {
-	  branchList <- preorderOU(branchList, phy, phy$edge[i,2], rootState, sqrt.alpha, theta) }}
+	  branchList <- preorderOU(branchList, phy, phy$edge[i,2], rootState, alpha, theta) }}
 if(model == "meanVar") branchList <- branchList
 
-value <- (list(branchList = branchList, timesList = timesList, steps = steps, parameters = list(rootState = rootState, sqrt.alpha = sqrt.alpha, variance = variance, theta = theta))) 
+value <- (list(branchList = branchList, timesList = timesList, steps = steps, parameters = list(rootState = rootState, alpha = alpha, variance = variance, theta = theta))) 
 class(value) <- "ouSim"
 return(value)
 }
